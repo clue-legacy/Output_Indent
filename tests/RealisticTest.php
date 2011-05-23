bdb03c9dcb11648f3c824ffd131ca9de215d4643
Christian Lück <christian@lueck.tv>
2011-05-23 15:05:48 +0200

    More realistic tests

--- tests/RealisticTest.php
+++ tests/RealisticTest.php
@@ -20,4 +20,32 @@ class RealisticTest extends PHPUnit_Framework_TestCase {
              "  b\r\n".
              "c";
     }
+    
+    public function testOneRepeated(){
+        $this->testOne();
+        $this->testOne();
+        $this->testOne();
+    }
+    
+    public function testTwo(){
+        ob_start();
+        
+        echo "two";
+        $indent = new Output_Indent('  ');
+        $this->subTwo();
+        unset($indent);
+        
+        $content = ob_get_flush();
+        $this->assertEquals("two\r\n  deeper\r\n    deepest",$content); 
+    }
+    
+    private function subTwo(){
+        echo "deeper";
+        $indent = new Output_Indent(2);
+        $this->subSubTwo();
+    }
+    
+    private function subSubTwo(){
+        echo "deepest";
+    }
 }
